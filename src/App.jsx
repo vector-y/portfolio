82934ac40cd2ce7ca29fd7e3059db1a4653e9730
Victor <vt.victortran@gmail.com>
2022-09-30 20:14:31 -0700

    Added component boilerplates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './style'
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
@@ -9,6 +10,21 @@ const App = () => (
             Navbar
           </div>
         </div>
+
+        {/* hero section */}
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Hero
+          </div>
+        </div>
+
+        {/* stats section */}
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Stats
+          </div>
+        </div>
+
     </div>
 );
 

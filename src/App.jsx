1468a6f729fc36183200dcd32ac7f0fbfe8f8584
Victor <vt.victortran@gmail.com>
2022-09-30 20:04:21 -0700

    Added basic file structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './style'
+
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          {/* gives full width */}
+          <div className={`${styles.boxWidth}`}> 
+            Navbar
+          </div>
+        </div>
+    </div>
+);
+
+export default App
